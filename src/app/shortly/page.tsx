cc3ad2dbee700103ba81294f1cc2e09d7fdbceea
Dor-J <Mr.DorJosef@gmail.com>
2025-06-09 17:31:55 +0300

    Add menu for desktop and  start hero section

--- src/app/shortly/page.tsx
+++ src/app/shortly/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import type { StaticImageData } from "next/image";
+
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -29,16 +31,72 @@ export default function ShortlyPage() {
             <Image src={logo} alt="shortly logo" />
             {/* Left Menu */}
             <div className="hidden space-x-8 font-bold lg:flex">
-              <a
-                href="#"
-                className="text-grayishViolet hover:text-veryDarkViolet"
-              >
+              <a href="#" className="text-violet-600 hover:text-violet-800">
                 Features
+              </a>
+              <a href="#" className="text-violet-600 hover:text-violet-800">
+                Pricing
+              </a>
+              <a href="#" className="text-violet-600 hover:text-violet-800">
+                Recources
               </a>
             </div>
           </div>
+
+          {/* Right buttons menu */}
+          <div className="hidden items-center space-x-6 font-bold text-violet-600 lg:flex">
+            <div className="hover:text-violet-800">Login</div>
+            <a
+              href="#"
+              className="rounded-full bg-cyan-400 px-8 py-3 font-bold text-white capitalize hover:opacity-70"
+            >
+              Sign Up
+            </a>
+          </div>
+          {/* Hambutger Menu */}
+          <button
+            id="menu-btn"
+            className="hamburger block focus:outline-none lg:hidden"
+            type="button"
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
         </div>
+        {/* TODOs Mobile Menu */}
       </nav>
+
+      {/* -----------Hero Section----------- */}
+
+      <section id="shortly-hero">
+        {/* Hero Container */}
+        <div className="container mx-auto flex flex-col-reverse p-6 lg:flex-row">
+          {/* Content container */}
+          <div className="mb-44 flex flex-col space-y-10 lg:mt-16 lg:w-1/2 xl:mb-52">
+            <h1 className="text-center text-5xl font-bold lg:max-w-md lg:text-left lg:text-6xl">
+              More than just shorter links
+            </h1>
+            <p className="text-center text-2xl text-gray-400 lg:max-w-md lg:text-left">
+              Build your brand&apos;s recognition and get detailed insights on
+              how your links are performing.
+            </p>
+            <div className="mx-auto lg:mx-0">
+              <a
+                href="#"
+                className="bg-cyan rounded-full px-10 py-5 text-2xl font-bold text-white hover:opacity-70 lg:py-4"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+
+          {/*  Image  */}
+          <div className="mx-auto mb-24 md:w-180 lg:mb-0 lg:w-1/2">
+            <Image src={heroSectionImage} alt="" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
