af0af703dde1bb49ac8e98fc62ab9729a7fc34e2
Dor-J <Mr.DorJosef@gmail.com>
2025-06-09 16:29:36 +0300

    Add configs for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // Shortly
+      colors: {
+        cyanClr: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        redClr: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [
+    // e.g. require('@tailwindcss/forms'),
+  ],
+};
